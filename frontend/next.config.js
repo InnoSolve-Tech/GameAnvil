777d5d1c1ae524932d9d284f18bef491471aa7ad
Benzi Darwin <ssalibenjamin0402@gmail.com>
2023-12-02 20:38:43 +0300

    First commit

---
+++ frontend/next.config.js
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-env node */
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+let nextConfig = {
+  // Fix for warnings about cjs/esm package duplication
+  // See: https://github.com/polkadot-js/api/issues/5636
+  transpilePackages: [
+    '**@polkadot/**',
+    '@polkadot/api',
+    '@polkadot/api-contract',
+    '@polkadot/extension-dapp',
+    '@polkadot/extension-inject',
+    '@polkadot/keyring',
+    '@polkadot/types',
+    '@polkadot/util',
+    '@polkadot/util-crypto',
+  ],
+}
+
+const withTwin = require('./withTwin.js')
+nextConfig = withTwin(nextConfig)
+
+module.exports = nextConfig
