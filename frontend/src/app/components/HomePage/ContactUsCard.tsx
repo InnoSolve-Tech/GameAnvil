1bb678933b9338089366681ce7a26a6d174190b5
BenziDarwin <ssalibenjamin0402@gmail.com>
2024-03-31 23:24:02 +0300

    Editing email input field

--- frontend/src/app/components/HomePage/ContactUsCard.tsx
+++ frontend/src/app/components/HomePage/ContactUsCard.tsx
@@ -26,11 +26,11 @@ function ContactUsCard() {
                 Got a question? We&apos;d love to hear from you. <br />
                 Send us a message and we&apos;ll respond as soon as possible.
               </p>
-              <div className="inline-block p-4">
+              <div className="inline-block">
                 <input
                   type="text"
                   placeholder="Your Email Address"
-                  className="w-80 rounded-lg border-0 bg-gray-800 px-4 py-3 text-white placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-500"
+                  className="w-80 rounded-lg border-0 bg-gray-800 px-6 py-3 text-white placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-500"
                   style={{ color: '#DBDBDB' }}
                 />
                 <button className="m-2 ml-4 rounded-lg bg-blue-500 px-4 py-3 font-bold text-white hover:bg-blue-700">
